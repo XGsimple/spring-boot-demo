c9dc16f89ca4e208281735af8c9ed7e44046938c
MintLemon <xugangmail@163.com>
2021-11-28 13:40:41 +0800

    使用Junit5

--- demo-orm-beetlsql/src/test/java/com/xkcoding/orm/beetlsql/service/UserServiceTest.java
+++ demo-orm-beetlsql/src/test/java/com/xkcoding/orm/beetlsql/service/UserServiceTest.java
@@ -1,9 +1,7 @@
 package com.xkcoding.orm.beetlsql.service;
 
-import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.json.JSONUtil;
 import com.xkcoding.orm.beetlsql.SpringBootDemoOrmBeetlsqlApplicationTests;
@@ -11,11 +9,14 @@ import com.xkcoding.orm.beetlsql.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 import org.beetl.sql.core.engine.PageQuery;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 /**
  * <p>
@@ -26,6 +27,7 @@ import java.util.List;
  * @date Created in 2018-11-14 16:30
  */
 @Slf4j
+@ExtendWith(SpringExtension.class)
 public class UserServiceTest extends SpringBootDemoOrmBeetlsqlApplicationTests {
     @Autowired
     private UserService userService;
@@ -33,10 +35,11 @@ public class UserServiceTest extends SpringBootDemoOrmBeetlsqlApplicationTests {
     @Test
     public void saveUser() {
         String salt = IdUtil.fastSimpleUUID();
-        User user = User.builder().name("testSave3").password(SecureUtil.md5("123456" + salt)).salt(salt).email("testSave3@xkcoding.com").phoneNumber("17300000003").status(1).lastLoginTime(new DateTime()).createTime(new DateTime()).lastUpdateTime(new DateTime()).build();
+        User user = User.builder().name("testSave3").password(SecureUtil.md5("123456" + salt)).salt(salt).email("testSave3@xkcoding.com")
+                        .phoneNumber("17300000003").status(1).lastLoginTime(new DateTime()).createTime(new DateTime()).lastUpdateTime(new DateTime()).build();
 
         user = userService.saveUser(user);
-        Assert.assertTrue(ObjectUtil.isNotNull(user.getId()));
+        assertThat(user.getId()).isNotNull();
         log.debug("【user】= {}", user);
     }
 
@@ -45,18 +48,20 @@ public class UserServiceTest extends SpringBootDemoOrmBeetlsqlApplicationTests {
         List<User> users = Lists.newArrayList();
         for (int i = 5; i < 15; i++) {
             String salt = IdUtil.fastSimpleUUID();
-            User user = User.builder().name("testSave" + i).password(SecureUtil.md5("123456" + salt)).salt(salt).email("testSave" + i + "@xkcoding.com").phoneNumber("1730000000" + i).status(1).lastLoginTime(new DateTime()).createTime(new DateTime()).lastUpdateTime(new DateTime()).build();
+            User user = User.builder().name("testSave" + i).password(SecureUtil.md5("123456" + salt)).salt(salt).email("testSave" + i + "@xkcoding.com")
+                            .phoneNumber("1730000000" + i).status(1).lastLoginTime(new DateTime()).createTime(new DateTime()).lastUpdateTime(new DateTime())
+                            .build();
             users.add(user);
         }
         userService.saveUserList(users);
-        Assert.assertTrue(userService.getUserList().size() > 2);
+        assertThat(userService.getUserList().size()).isGreaterThan(2);
     }
 
     @Test
     public void deleteUser() {
         userService.deleteUser(1L);
         User user = userService.getUser(1L);
-        Assert.assertTrue(ObjectUtil.isNull(user));
+        assertThat(user).isNull();
     }
 
     @Test
@@ -64,21 +69,21 @@ public class UserServiceTest extends SpringBootDemoOrmBeetlsqlApplicationTests {
         User user = userService.getUser(2L);
         user.setName("beetlSql 修改后的名字");
         User update = userService.updateUser(user);
-        Assert.assertEquals("beetlSql 修改后的名字", update.getName());
+        assertThat("beetlSql 修改后的名字").isEqualTo(update.getName());
         log.debug("【update】= {}", update);
     }
 
     @Test
     public void getUser() {
         User user = userService.getUser(1L);
-        Assert.assertNotNull(user);
+        assertThat(user).isNotNull();
         log.debug("【user】= {}", user);
     }
 
     @Test
     public void getUserList() {
         List<User> userList = userService.getUserList();
-        Assert.assertTrue(CollUtil.isNotEmpty(userList));
+        assertThat(userList).isNotEmpty();
         log.debug("【userList】= {}", userList);
     }
 
@@ -86,8 +91,8 @@ public class UserServiceTest extends SpringBootDemoOrmBeetlsqlApplicationTests {
     public void getUserByPage() {
         List<User> userList = userService.getUserList();
         PageQuery<User> userByPage = userService.getUserByPage(1, 5);
-        Assert.assertEquals(5, userByPage.getList().size());
-        Assert.assertEquals(userList.size(), userByPage.getTotalRow());
+        assertThat(userByPage.getList().size()).isEqualTo(5);
+        assertThat(userList.size()).isEqualTo(userByPage.getTotalRow());
         log.debug("【userByPage】= {}", JSONUtil.toJsonStr(userByPage));
     }
 }
