f070fdf40083596f008dbc6c44f8ac5447a06032
MintLemon <xugangmail@163.com>
2023-07-09 17:13:28 +0800

    延迟处理

---
+++ mydemo-delay/src/main/java/com/xkcoding/bf/DelayApplication.java
@@ -0,0 +1,16 @@
+package com.xkcoding.bf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * https://mp.weixin.qq.com/s/AWw19E8iWZnrx-SDgWN4bQ
+ */
+@SpringBootApplication
+public class DelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayApplication.class, args);
+    }
+
+}
